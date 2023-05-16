ba366b59ca37d4278042bb693045a1861c691639
phitran <phi.tran@saigontechnology.com>
2023-05-16 10:12:26 +0700

    csa

--- App.tsx
+++ App.tsx
@@ -53,7 +53,7 @@ function App({isShow}: Props): any {
   //     textLightColor: '#333333',
   //   },
   // });
-  console.log('ccccc 58');
+  console.log('ccccc 59');
 
   return (
     <View>{isShow && <Text testID="label">Label</Text>}</View>
